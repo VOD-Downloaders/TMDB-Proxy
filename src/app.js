11093709abb12571ca799fa78ca1cd61f2906a2b
ggjorven <jorbenvanderwal@gmail.com>
2026-06-08 16:14:46 +0200

    Comment TMDB_API_KEY in javascript since unused.

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config({ path: ".env.local" });
 
 const TMDB_BASE_URL = "https://api.themoviedb.org";
-const TMDB_API_KEY = process.env.TMDB_API_KEY;
+// const TMDB_API_KEY = process.env.TMDB_API_KEY;
 const TMDB_READ_ACCESS_TOKEN = process.env.TMDB_READ_ACCESS_TOKEN;
 
 const app = express();
