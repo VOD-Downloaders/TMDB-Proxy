12effd90d14dfe2ea2b7e6aa45d67e84cfbd19e9
ggjorven <jorbenvanderwal@gmail.com>
2026-06-08 16:12:00 +0200

    Fix format string using wrong quotes.

--- src/local.js
+++ src/local.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-	console.log("TMDB proxy running on http://localhost:${PORT}");
+	console.log(`TMDB proxy running on http://localhost:${PORT}`);
 });
